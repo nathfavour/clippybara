0fac24f9357396c610e689db0d95dd940762430b
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-18 16:37:32 +0100

    🧵 Add custom widget to lib/widgets/custom_widget.

--- lib/widgets/custom_widget.dart
+++ lib/widgets/custom_widget.dart
@@ -1,26 +1,25 @@
+import 'package:flutter/material.dart';
 
+class CustomSwitchTile extends StatelessWidget {
+  final String title;
+  final bool value;
+  final ValueChanged<bool> onChanged;
 
+  const CustomSwitchTile({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}  }    );      ),        onChanged: onChanged,        value: value,      trailing: Switch(      title: Text(title),    return ListTile(  Widget build(BuildContext context) {  @override  });    required this.onChanged,    required this.value,    required this.title,    super.key,  const CustomSwitchTile({  final ValueChanged<bool> onChanged;  final bool value;  final String title;class CustomSwitchTile extends StatelessWidget {import 'package:flutter/material.dart';
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      trailing: Switch(
+        value: value,
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
